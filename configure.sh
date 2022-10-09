9f1346e23d5dba1082a44cdee8dd0713790b99fb
aron-s <aronshrestha331@gmail.com>
2022-10-09 18:17:31 -0400

    config

--- configure.sh
+++ configure.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID="${{ secrets.AWS_ACCESS_KEY_ID }}"
-export AWS_SECRET_ACCESS_KEY="${{ secrets.AWS_SECRET_ACCESS_KEY }}"
-export AWS_DEFAULT_REGION=us-east-1
-
 set -e
 PROFILE_NAME=everypenny
 CLUSTER_NAME=everypenny-cluster
--- create-cluster.sh
+++ create-cluster.sh
@@ -3,8 +3,9 @@ KEY_PAIR=everypenny-cluster
     ecs-cli up \
       --keypair $KEY_PAIR  \
       --capability-iam \
-      --size 1 \
+      --size 2 \
       --instance-type t2.micro \
       --tags project=everypenny-cluster,owner=CS490 \
       --cluster-config everypenny \
-      --ecs-profile everypenny
+      --ecs-profile everypenny \
+      --force
