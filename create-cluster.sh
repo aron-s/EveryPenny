773224598933d7adeedd08b4b8fb10ae34881134
Jihadul Islam <jislam0513@gmail.com>
2022-10-12 22:42:08 -0400

    new build images

--- create-cluster.sh
+++ create-cluster.sh
@@ -3,7 +3,7 @@ KEY_PAIR=everypenny-cluster
     ecs-cli up \
       --keypair $KEY_PAIR  \
       --capability-iam \
-      --size 2 \
+      --size 8 \
       --instance-type t2.micro \
       --tags project=everypenny-cluster,owner=CS490 \
       --cluster-config everypenny \
